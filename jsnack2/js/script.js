507907caa0ffdabf2761bfa5f8908b9b0d3f20b5
Christefan <chriscapod7@gmail.com>
2022-04-20 17:34:14 +0200

    Correzione jsnack2

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -4,7 +4,7 @@ let cont1;
 let cont2;
 for(let i = 0;i < 3; i++){
     cont1 = Math.floor(Math.random() * username.length);
-    cont2 = Math.floor(Math.random() * username.length);
+    cont2 = Math.floor(Math.random() * prename.length);
     console.log(username[cont1],prename[cont2]);
 
 }
